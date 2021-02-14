e4dd54767819ab0f7cb6fcdfe12143b89d8cfa42
MattAgn <matthieua@bam.tech>
2021-02-14 12:52:01 +0100

    feat: add redirection from home to rooms

--- lib/code_name_web/live/home_live.ex
+++ lib/code_name_web/live/home_live.ex
@@ -6,4 +6,22 @@ defmodule CodeNameWeb.HomeLive do
     socket = assign(socket, player_nickname: "")
     {:ok, socket}
   end
+
+  @impl true
+  def handle_event("player-nickname-choice", %{"player_nickname" => player_nickname}, socket) do
+    # TODO: handle unicity / id of player nicknames
+    IO.puts(player_nickname)
+
+    socket =
+      push_redirect(socket,
+        to:
+          Routes.live_path(
+            socket,
+            CodeNameWeb.RoomsLive,
+            player_nickname: player_nickname
+          )
+      )
+
+    {:noreply, socket}
+  end
 end
