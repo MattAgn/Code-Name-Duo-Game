7bda38f540281b8aa8bee094ee4e68be28320f1c
MattAgn <matthieua@bam.tech>
2021-02-14 13:04:11 +0100

    feat: generate room token

--- lib/code_name_web/live/rooms_live.ex
+++ lib/code_name_web/live/rooms_live.ex
@@ -5,4 +5,15 @@ defmodule CodeNameWeb.RoomsLive do
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
+
+  @impl true
+  def handle_event("create-room", _, socket) do
+    room_token =
+      RandomStringGenerator.generate("LLLllllddddd")
+      |> RandomStringGenerator.shuffle()
+
+    socket = assign(socket, room_token: room_token)
+
+    {:noreply, socket}
+  end
 end
